b655238b9f9879b11cad81372722cdd60ac4d488
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2021-07-20 17:33:14 +0500

    typeguard instanceof

--- type-guards/index.js
+++
@@ -1,9 +0,0 @@
-function logError(error) {
-    if (typeof error === "string") {
-        //this is typeguard
-        console.log(error);
-    }
-    else {
-        console.log(error.message);
-    }
-}
--- type-guards/index.ts
+++ type-guards/index.ts
@@ -6,3 +6,20 @@ function logError(error: string | Error): void {
     console.log(error.message)
   }
 }
+
+export class Rectangle {
+  constructor(public width: number, public height: number) {}
+}
+
+export class Circle {
+  constructor(public radius: number) {}
+}
+
+function getArea(geometry: Rectangle | Circle): number {
+  if (geometry instanceof Circle) {
+    //Instanceof is also a type guard in typescript
+    return Math.PI * Math.pow(geometry.radius, 2)
+  } else {
+    return geometry.height * geometry.width
+  }
+}
